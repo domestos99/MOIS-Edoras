eb5a178b488ed4aa9fb61cd8b565cc903d4160e0
Domca-HP <plasido1@uhk.cz>
2019-02-28 16:19:30 +0100

    JS payment - oprava typu id; pridani routing na detail

--- Edoras.JS/js/src/app/layouts/secure/secure.routes.ts
+++ Edoras.JS/js/src/app/layouts/secure/secure.routes.ts
@@ -3,6 +3,7 @@ import {AuthGuard} from "@app/core";
 import {PaymentComponent} from "@app/modules/secure/payment/payment.component";
 import {DashboardComponent} from "@app/modules/secure/dashboard/dashboard.component";
 import {TestComponent} from "@app/modules/secure/test/test.component";
+import {PaymentDetailComponent} from "@app/modules/secure/payment/detail/payment-detail.component";
 
 export const SECURE_ROUTES: Routes = [
 
@@ -10,5 +11,6 @@ export const SECURE_ROUTES: Routes = [
   {path: '', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: 'payments', component: PaymentComponent, canActivate: [AuthGuard]},
+  {path: 'payments/:id', component: PaymentDetailComponent, canActivate: [AuthGuard]},
   {path: 'test', component: TestComponent, canActivate: [AuthGuard]}
 ];
