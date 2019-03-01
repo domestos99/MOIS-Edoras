993c8aef39c6f9cddd991a35bd2208f33d466f3d
OndrejP <34539494+pipekon@users.noreply.github.com>
2019-03-01 10:29:59 +0100

    Merge branch 'master' of https://github.com/domestos99/MOIS-Edoras
    # Conflicts:
    #	Edoras.JS/js/src/app/layouts/secure/secure.routes.ts

--- Edoras.JS/js/src/app/layouts/secure/secure.routes.ts
+++ Edoras.JS/js/src/app/layouts/secure/secure.routes.ts
@@ -10,6 +10,7 @@ export const SECURE_ROUTES: Routes = [
   {path: '', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: 'payments', component: PaymentComponent, canActivate: [AuthGuard]},
+  {path: 'payments/:id', component: PaymentDetailComponent, canActivate: [AuthGuard]},
   {path: 'categories', component: CategoryComponent, canActivate: [AuthGuard]},
   {path: 'test', component: TestComponent, canActivate: [AuthGuard]}
 ];
