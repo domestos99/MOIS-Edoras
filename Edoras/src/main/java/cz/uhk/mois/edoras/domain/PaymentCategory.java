7e6e06a1c06ebe70c300dce4033d1b5bb5ad3d9e
Dominik-PC <domestos99@seznam.cz>
2019-03-01 22:14:55 +0100

    Add PaymentCategory; TransactionCategory column constrains

--- Edoras/src/main/java/cz/uhk/mois/edoras/domain/PaymentCategory.java
+++ Edoras/src/main/java/cz/uhk/mois/edoras/domain/PaymentCategory.java
@@ -7,8 +7,13 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"paymentId"})
+})
 public class PaymentCategory implements Serializable
 {
     @Id
