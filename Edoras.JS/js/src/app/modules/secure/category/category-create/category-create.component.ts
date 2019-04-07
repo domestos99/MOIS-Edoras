2e926833d1f39407f78dbe9f7f97fdd0ff9b9ac8
Dominik-PC <domestos99@seznam.cz>
2019-04-07 20:12:40 +0200

    JS - category-create update design + validations

--- Edoras.JS/js/src/app/modules/secure/category/category-create/category-create.component.ts
+++ Edoras.JS/js/src/app/modules/secure/category/category-create/category-create.component.ts
@@ -1,19 +1,51 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material";
 import {CategoryCreateModel} from "@app/modules/secure/category/category-create/category-create.model";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
   selector: 'category-create',
-  templateUrl: 'category-create.component.html'
+  templateUrl: 'category-create.component.html',
+  styleUrls: ['./category-create.component.css']
 })
-export class CategoryCreateComponent {
+export class CategoryCreateComponent implements OnInit {
 
   constructor(
+    private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<CategoryCreateComponent>,
     @Inject(MAT_DIALOG_DATA) public data: CategoryCreateModel) {
+  }
 
+  inputForm: FormGroup;
+  submitted = false;
 
+  ngOnInit() {
+    this.inputForm = this.formBuilder.group({
+      catename: [this.data.name, Validators.required],
+      cateicon: [this.data.icon, Validators.required],
+      catetype: [this.data.type, Validators.required],
+    });
+
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.inputForm.controls;
+  }
+
+  onOkClick() {
+
+    if (this.isDataValid()) {
+      this.dialogRef.close(this.data);
+    } else {
+      this.submitted = true;
+      // stop here if form is invalid
+      if (this.inputForm.invalid) {
+        return;
+      }
+
+    }
   }
 
   iconsList = [
@@ -37,4 +69,7 @@ export class CategoryCreateComponent {
   }
 
 
+  private isDataValid() {
+    return (this.data && this.data.name && this.data.type && this.data.icon);
+  }
 }
