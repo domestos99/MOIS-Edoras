e1c7c0a61b7a0661b7b0d48d8835d599cffed718
Luboš Hajek <hajeklu@outlook.com>
2019-04-16 22:10:13 +0200

    Transaction sorting.

--- Edoras/src/main/java/cz/uhk/mois/edoras/dao/TransactionDAO.java
+++ Edoras/src/main/java/cz/uhk/mois/edoras/dao/TransactionDAO.java
@@ -1,8 +1,18 @@
 package cz.uhk.mois.edoras.dao;
 
+import cz.uhk.mois.edoras.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cz.uhk.mois.edoras.bankingapi.model.Transaction;
 
+import java.util.Date;
+import java.util.List;
+
 public interface TransactionDAO extends JpaRepository<Transaction, String> {
+
+    List<Transaction> findByValueDateBetweenOrderByValueDateDesc(Date from, Date to);
+
+    List<Transaction> findByCategoryOrderByValueDateDesc(Category category);
+
+    List<Transaction> findByValueDateBetweenAndCategoryOrderByValueDateDesc(Date from, Date to, Category category);
 }
--- Edoras/src/main/java/cz/uhk/mois/edoras/web/dto/TransactionFilterModel.java
+++ Edoras/src/main/java/cz/uhk/mois/edoras/web/dto/TransactionFilterModel.java
@@ -2,44 +2,41 @@ package cz.uhk.mois.edoras.web.dto;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.io.Serializable;
 import java.util.Date;
 
-public class TransactionFilterModel implements Serializable
-{
+public class TransactionFilterModel implements Serializable {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @Temporal(TemporalType.TIMESTAMP)
     private Date dtFrom;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @Temporal(TemporalType.TIMESTAMP)
     private Date dtTo;
     private String cateId;
 
-    public Date getDtFrom()
-    {
+    public Date getDtFrom() {
         return dtFrom;
     }
 
-    public void setDtFrom(Date dtFrom)
-    {
+    public void setDtFrom(Date dtFrom) {
         this.dtFrom = dtFrom;
     }
 
-    public Date getDtTo()
-    {
+    public Date getDtTo() {
         return dtTo;
     }
 
-    public void setDtTo(Date dtTo)
-    {
+    public void setDtTo(Date dtTo) {
         this.dtTo = dtTo;
     }
 
-    public String getCateId()
-    {
+    public String getCateId() {
         return cateId;
     }
 
-    public void setCateId(String cateId)
-    {
+    public void setCateId(String cateId) {
         this.cateId = cateId;
     }
 }
